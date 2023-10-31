577104e982b99bec5dda62e3c4ceadafb6a74682
czl <1870185035@qq.com>
2023-10-31 15:10:21 +0800

    10.31 15:09

---
+++ app/src/main/java/com/example/hotelmanager/bean/Item.java
@@ -0,0 +1,49 @@
+package com.example.hotelmanager.bean;
+
+import java.util.Date;
+
+public class Item {
+    String item_name;//物品名称
+    String item_type;//物品类型
+    String item_position;//存放位置
+    int item_number;//物品数量,
+    Date date_of_manufacture;//生产日期
+    int quality_guarantee_period;//保质期
+    int remind_days;//提醒日期
+    public Item(String item_name,String item_type,String item_position,int item_number,Date date_of_manufacture,int quality_guarantee_period,int remind_days){
+        this.item_name=item_name;
+        this.item_type=item_type;
+        this.item_position=item_position;
+        this.item_number=item_number;
+        this.date_of_manufacture=date_of_manufacture;
+        this.quality_guarantee_period=quality_guarantee_period;
+        this.remind_days=remind_days;
+    }
+    public String getItem_name() {return item_name;}
+
+    public void setItem_name(String item_name) {this.item_name=item_name;}
+
+    public String getItem_type() {return item_type;}
+
+    public void setItem_type(String item_type) {this.item_type=item_type;}
+
+    public String getItem_position() {return item_position;}
+
+    public void setItem_position(String item_position) {this.item_position=item_position;}
+
+    public int getItem_number() {return item_number;}
+
+    public void setItem_number(int item_number) {this.item_number=item_number;}
+
+    public Date getDate_of_manufacture() {return date_of_manufacture;}
+
+    public void setDate_of_manufacture(Date date_of_manufacture) {this.date_of_manufacture=date_of_manufacture;}
+
+    public int getQuality_guarantee_period() {return quality_guarantee_period;}
+
+    public void setQuality_guarantee_period(int quality_guarantee_period) {this.quality_guarantee_period=quality_guarantee_period;}
+
+    public int getRemind_days() {return remind_days;}
+
+    public void setRemind_days(int remind_days) {this.remind_days=remind_days;}
+}

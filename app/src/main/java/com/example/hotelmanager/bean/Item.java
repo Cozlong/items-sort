17526c8bcd6607c8658032e0841b11c3cdbd6fc0
zmj <2339349805@qq.com>
2023-11-01 14:39:29 +0800

    item的更改

--- app/src/main/java/com/example/hotelmanager/bean/Item.java
+++ app/src/main/java/com/example/hotelmanager/bean/Item.java
@@ -8,13 +8,12 @@ public class Item {
     String item_type;//物品类型
     String item_position;//存放位置
     String term_type;//期限类型
-    int item_number;//物品数量,
-
-    Date date_now;//登记日期
+    int item_number;//物品数量
+    String date_now;//登记日期
     Date date_of_manufacture;//生产日期
     int quality_guarantee_period;//保质期
     int remind_days;//提醒日期
-    public Item(String img,String item_name,String item_type,String item_position,String term_type,int item_number,Date date_now,Date date_of_manufacture,int quality_guarantee_period,int remind_days){
+    public Item(String img,String item_name,String item_type,String item_position,String term_type,int item_number,String date_now,Date date_of_manufacture,int quality_guarantee_period,int remind_days){
         this.img=img;
         this.item_name=item_name;
         this.item_type=item_type;
@@ -48,9 +47,9 @@ public class Item {
     public int getItem_number() {return item_number;}
 
     public void setItem_number(int item_number) {this.item_number=item_number;}
-    public Date getDate_now() {return date_now;}
+    public String getDate_now() {return date_now;}
 
-    public void setDate_now(Date date_now) {this.date_now=date_now;}
+    public void setDate_now(String date_now) {this.date_now=date_now;}
 
     public Date getDate_of_manufacture() {return date_of_manufacture;}
 

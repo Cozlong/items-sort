8f3cb42a15b7525033e63182765d5a3fcc546e16
czl <1870185035@qq.com>
2023-11-02 10:27:19 +0800

    change

--- app/src/main/java/com/example/hotelmanager/data/ItemDBHelper.java
+++ app/src/main/java/com/example/hotelmanager/data/ItemDBHelper.java
@@ -8,32 +8,47 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ItemDBHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAEM="item_db";
-    private static final String TABLE_NAEM="item";
+    private static final String DATABASE_NAME="item_db";
+
+    private static final String TABLE_NAME1="item";
+    private static final String TABLE_NAME2="user";
+    private static final String TABLE_NAME3="type";
     private static final int DATABASE_VERSION=1;
 
     public ItemDBHelper(Context context){
-        super(context,DATABASE_NAEM,null,DATABASE_VERSION);
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String strSQL="create table "+TABLE_NAEM
+        String strSQL1="create table "+TABLE_NAME1
                 +"(item_id integer primary key autoincrement,"
                 +"img varchar(100),item_name varchar(100),"
                 +"item_type varchar(100),item_position varchar(100),"
                 +"term_type varchar(100),item_number integer,"
                 +"date_now varchar(100),date_of_manufacture date,"
                 +"quality_guarantee_period integer,remind_days interger)";
-        db.execSQL(strSQL);
-        String sql="insert into "+TABLE_NAEM+"(item_name,item_type,item_position,term_type,item_number,date_now,date_of_manufacture,quality_guarantee_period,remind_days)"
+        db.execSQL(strSQL1);
+        String sql="insert into "+TABLE_NAME1+"(item_name,item_type,item_position,term_type,item_number,date_now,date_of_manufacture,quality_guarantee_period,remind_days)"
                 +"values('牛奶','食品','抽屉','未到期','10',"+getTime()+",'2023-03-12',60,20)";
         db.execSQL(sql);
-         sql="insert into "+TABLE_NAEM+"(item_name,item_type,item_position,term_type,item_number,date_now,date_of_manufacture,quality_guarantee_period,remind_days)"
+         sql="insert into "+TABLE_NAME1+"(item_name,item_type,item_position,term_type,item_number,date_now,date_of_manufacture,quality_guarantee_period,remind_days)"
                 +"values('消炎药','医药','桌子上','过期','1',"+getTime()+",'2023-02-12',360,180)";
         db.execSQL(sql);
-         sql="insert into "+TABLE_NAEM+"(item_name,item_type,item_position,term_type,item_number,date_now,date_of_manufacture,quality_guarantee_period,remind_days)"
+         sql="insert into "+TABLE_NAME1+"(item_name,item_type,item_position,term_type,item_number,date_now,date_of_manufacture,quality_guarantee_period,remind_days)"
                 +"values('洗面奶','洗漱','桶里','临期','1',"+getTime()+",'2023-02-12',180,80)";
+        db.execSQL(sql);
+        String strSQL2="create table "+TABLE_NAME2
+                +"(img varchar(100),id varchar(100),"
+                +"user_name varchar(100),pwd varchar(100),"
+                +"phone_number varchar(100),"
+                +"qq varchar(100),wechat varchar(100))";
+        db.execSQL(strSQL2);
+        sql="insert into "+TABLE_NAME2+"(img,id,user_name,pwd,phone_number,qq,wechat)"
+                +"values('a.png','czl','czlong','123','1234','11','22')";
+        db.execSQL(sql);
+        sql="insert into "+TABLE_NAME2+"(img,id,user_name,pwd,phone_number,qq,wechat)"
+                +"values('b.png','zmj','zmjia','456','5678','33','44')";
         db.execSQL(sql);
     }
 

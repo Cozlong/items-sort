6d20739832d76a87a2b9481ae4ed505fac507b0b
czl <1870185035@qq.com>
2023-11-02 14:45:51 +0800

    change

--- app/src/main/java/com/example/hotelmanager/data/ItemDBHelper.java
+++ app/src/main/java/com/example/hotelmanager/data/ItemDBHelper.java
@@ -1,6 +1,7 @@
 package com.example.hotelmanager.data;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -33,22 +34,22 @@ public class ItemDBHelper extends SQLiteOpenHelper {
                 +"values('牛奶','食品','抽屉','未到期','10','"+getTime()+"','2023-03-12',60,20)";
         db.execSQL(sql);
          sql="insert into "+TABLE_NAME1+"(item_name,item_type,item_position,term_type,item_number,date_now,date_of_manufacture,quality_guarantee_period,remind_days)"
-                +"values('消炎药','医药','桌子上','过期','1','"+getTime()+"'','2023-02-12',360,180)";
+                +"values('消炎药','医药','桌子上','过期','1','"+getTime()+"','2023-02-12',360,180)";
         db.execSQL(sql);
          sql="insert into "+TABLE_NAME1+"(item_name,item_type,item_position,term_type,item_number,date_now,date_of_manufacture,quality_guarantee_period,remind_days)"
                 +"values('洗面奶','洗漱','桶里','临期','1','"+getTime()+"','2023-02-12',180,80)";
         db.execSQL(sql);
         String strSQL2="create table "+TABLE_NAME2
-                +"(img varchar(100),id varchar(100),"
-                +"user_name varchar(100),pwd varchar(100),"
+                +"(id varchar(100),pwd varchar(100),"
+                +"user_name varchar(100),img varchar(100),"
                 +"phone_number varchar(100),"
                 +"qq varchar(100),wechat varchar(100))";
         db.execSQL(strSQL2);
-        sql="insert into "+TABLE_NAME2+"(img,id,user_name,pwd,phone_number,qq,wechat)"
-                +"values('a.png','czl','czlong','123','1234','11','22')";
+        sql="insert into "+TABLE_NAME2+"(id,pwd,img,user_name,phone_number,qq,wechat)"
+                +"values('czl','123','czlong','a.png','1234','11','22')";
         db.execSQL(sql);
         sql="insert into "+TABLE_NAME2+"(img,id,user_name,pwd,phone_number,qq,wechat)"
-                +"values('b.png','zmj','zmjia','456','5678','33','44')";
+                +"values('zmj','456','zmjia','b.png','5678','33','44')";
         db.execSQL(sql);
     }
 
